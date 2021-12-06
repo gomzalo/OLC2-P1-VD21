b158a228590201ae1b18734188783388ba9ffd5c
Ebenja <201504530@ingenieria.usac.edu.gt>
2021-12-05 22:24:59 -0600

    Simbolo

--- Clases/TablaSimbolos/Simbolo.ts
+++ Clases/TablaSimbolos/Simbolo.ts
@@ -1,4 +1,55 @@
 class Simbolo{
-    private identificador:number;
-    private tipo:tipo
+    private id;
+    private tipo:TIPO;
+    private fila: number;
+    private col: number;
+    private valor : any;
+    private arreglo : any;
+    private structEnv: any; // ENTORNO STRUCT
+    
+    constructor(id,tipo,arreglo,fila,col,valor,structEnv = null){
+        this.id = id;
+        this.tipo = tipo;
+        this.fila = fila;
+        this.col = col;
+        this.valor =  valor;
+        this.arreglo = arreglo;
+        this.structEnv = structEnv;
+    }
+
+    getId(){
+        return this.id;
+    }
+
+    setId(id){
+        this.id = id;
+    }
+
+    getTipo(){
+        return this.tipo;
+    }
+
+    setTipo(tipo){
+        this.tipo = tipo
+    }
+
+    getValor(){
+        return this.valor;
+    }
+
+    setValor(valor){
+        this.valor = valor;
+    }
+
+    getFila(){
+        return this.fila;
+    }
+
+    getColumn(){
+        return this.col;
+    }
+
+    getArreglo(){
+        return this.arreglo;
+    }
 }
